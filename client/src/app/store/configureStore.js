6e6897417b4e0f864a1664867b591f9176167414
Martin Metodiev <metodiev.martin@gmail.com>
2018-07-20 19:48:24 +0300

    Add comment labels in configureStore.js

--- client/src/app/store/configureStore.js
+++ client/src/app/store/configureStore.js
@@ -2,13 +2,18 @@
 
 
 
-// Import
+//--------------------------| Import
+
+// Redux
 import { createStore, combineReducers } from 'redux';
+
+// Reducers
 import devicesReducer from '../reducers/devices';
 import roomsReducer from '../reducers/rooms';
 import switchersReducer from '../reducers/switchers';
 
 
+//--------------------------| Export
 
 export default () => createStore(
   combineReducers({
