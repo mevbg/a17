8745e63a16414764977fab7262dcbeac00f01e9a
Martin Metodiev <metodiev.martin@gmail.com>
2022-07-23 18:27:38 +0300

    Fix import of TerserPlugin

--- configs/prod/webpack.config.prod.js
+++ configs/prod/webpack.config.prod.js
@@ -6,10 +6,12 @@
 
 const glob = require('glob');
 const path = require('path');
-const loaders = glob.sync(`${__dirname}/loaders/**/*.js`).map(file => require( path.resolve( file ) ));
-const plugins = glob.sync(`${__dirname}/plugins/**/*.js`).map(file => require( path.resolve( file ) ));
-const commonLoaders = glob.sync(`${__dirname}/../common/loaders/**/*.js`).map(file => require( path.resolve( file ) ));
-const commonPlugins = glob.sync(`${__dirname}/../common/plugins/**/*.js`).map(file => require( path.resolve( file ) ));
+const TerserPlugin = require('terser-webpack-plugin');
+
+const loaders = glob.sync(`${__dirname}/loaders/**/*.js`).map(file => require(path.resolve(file)));
+const plugins = glob.sync(`${__dirname}/plugins/**/*.js`).map(file => require(path.resolve(file)));
+const commonLoaders = glob.sync(`${__dirname}/../common/loaders/**/*.js`).map(file => require(path.resolve(file)));
+const commonPlugins = glob.sync(`${__dirname}/../common/plugins/**/*.js`).map(file => require(path.resolve(file)));
 
 
 //--------------------------| Configuration
